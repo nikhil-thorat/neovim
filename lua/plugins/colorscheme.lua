8884b4b44ddd192397b04efd3a7af3b5a8911633
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-03-03 20:44:40 +0530

    changed colorscheme to Mellow

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,31 +1,8 @@
 return {
-	"rebelot/kanagawa.nvim",
+	"mellow-theme/mellow.nvim",
 	priority = 1000,
 	config = function()
-		require("kanagawa").setup({
-			terminalColors = true,
-			transparent = true,
-			theme = "dragon",
-			colors = {
-				theme = {
-					all = {
-						ui = {
-							bg_gutter = "none",
-						},
-					},
-				},
-			},
-			overrides = function(colors)
-				return {
-					NormalFloat = { bg = "none" },
-					FloatBorder = { bg = "none" },
-					FloatTitle = { bg = "none" },
-					CursorLine = { bg = "none" },
-					CursorLineNr = { bg = "none" },
-				}
-			end,
-		})
-
-		vim.cmd("colorscheme kanagawa-dragon")
+		vim.g.mellow_transparent = true
+		vim.cmd("colorscheme mellow")
 	end,
 }
