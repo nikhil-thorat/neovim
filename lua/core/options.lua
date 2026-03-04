e6124bd8520c7c5618d58a66433391dd293cda18
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-03-04 21:59:11 +0530

    disabled line-numbers

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -19,7 +19,7 @@ vim.opt.cursorline = true
 -- vim.opt.scrolloff = 100
 -- vim.opt.sidescrolloff = 100
 
-vim.opt.number = true
+vim.opt.number = false
 vim.opt.relativenumber = false
 vim.opt.signcolumn = "no"
 vim.opt.wrap = false
