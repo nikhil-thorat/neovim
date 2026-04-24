5311b86fa3f91b77ff3742d3e6b96a08ea3eb8bd
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-04-24 20:58:46 +0530

    Changed colorscheme to batman

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-	"stealth-theme",
+	"batman",
 	dir = "~/.config/nvim/colors",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd([[colorscheme stealth]])
+		vim.cmd([[colorscheme batman]])
 	end,
 }
