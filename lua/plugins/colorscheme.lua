ee6aa6d4c232b239fdd2a451fa6a9c61d425ce7a
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-05-29 11:33:27 +0000

    change theme options

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,10 +9,32 @@ return {
             statementStyle = { bold = true },
             transparent = true,
             terminalColors = true,
+            colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
+            overrides = function(colors)
+                local theme = colors.theme
+                return {
+                    NormalFloat = { bg = "none" },
+                    FloatBorder = { bg = "none" },
+                    FloatTitle = { bg = "none" },
+                    NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+                    LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                    MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+                    Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+                    PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                    PmenuSbar = { bg = theme.ui.bg_m1 },
+                    PmenuThumb = { bg = theme.ui.bg_p2 },
+
+                    TelescopeTitle = { fg = theme.ui.special, bold = true },
+                    TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+                    TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+                    TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+                    TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+                    TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+                    TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+                }
+            end
         })
-        vim.cmd("colorscheme kanagawa")
-        vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "NONE" })
+        vim.cmd("colorscheme kanagawa-dragon")
     end
 }
