e9c9e0794df7d4d613d9735b167a93dc2ff03ba9
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-06-27 19:47:42 +0530

    changed colorscheme to everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,40 +1,103 @@
 return {
-    "rebelot/kanagawa.nvim",
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
     priority = 1000,
+
     config = function()
-        require('kanagawa').setup({
-            commentStyle = { italic = true },
-            functionStyle = { italic = false },
-            keywordStyle = { italic = false },
-            statementStyle = { bold = true },
-            transparent = true,
-            terminalColors = true,
-            colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
-            overrides = function(colors)
-                local theme = colors.theme
-                return {
-                    NormalFloat = { bg = "none" },
-                    FloatBorder = { bg = "none" },
-                    FloatTitle = { bg = "none" },
-                    NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-                    LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                    MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+        require("everforest").setup({
+            background = "hard",
+            transparent_background_level = 0,
 
-                    Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
-                    PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-                    PmenuSbar = { bg = theme.ui.bg_m1 },
-                    PmenuThumb = { bg = theme.ui.bg_p2 },
+            italics = false,
+            disable_italic_comments = false,
 
-                    TelescopeTitle = { fg = theme.ui.special, bold = true },
-                    TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-                    TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-                    TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-                    TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-                    TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-                    TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+            on_highlights = function(hl, palette)
+                hl.NormalFloat = { bg = "NONE" }
+                hl.FloatBorder = { bg = "NONE" }
+                hl.FloatTitle = { bg = "NONE" }
+
+                hl.NormalDark = {
+                    fg = palette.grey1,
+                    bg = palette.bg_dim,
                 }
-            end
+
+                hl.LazyNormal = {
+                    fg = palette.grey1,
+                    bg = palette.bg_dim,
+                }
+
+                hl.MasonNormal = {
+                    fg = palette.grey1,
+                    bg = palette.bg_dim,
+                }
+
+                -- Completion Menu
+                hl.Pmenu = {
+                    fg = palette.fg,
+                    bg = palette.bg3,
+                }
+
+                hl.PmenuSel = {
+                    fg = "NONE",
+                    bg = palette.bg5,
+                }
+
+                hl.PmenuSbar = {
+                    bg = palette.bg2,
+                }
+
+                hl.PmenuThumb = {
+                    bg = palette.bg5,
+                }
+
+                -- Telescope
+                hl.TelescopeTitle = {
+                    fg = palette.green,
+                    bold = true,
+                }
+
+                hl.TelescopePromptNormal = {
+                    bg = palette.bg3,
+                }
+
+                hl.TelescopePromptBorder = {
+                    fg = palette.bg3,
+                    bg = palette.bg3,
+                }
+
+                hl.TelescopeResultsNormal = {
+                    fg = palette.grey1,
+                    bg = palette.bg_dim,
+                }
+
+                hl.TelescopeResultsBorder = {
+                    fg = palette.bg_dim,
+                    bg = palette.bg_dim,
+                }
+
+                hl.TelescopePreviewNormal = {
+                    bg = palette.bg0,
+                }
+
+                hl.TelescopePreviewBorder = {
+                    fg = palette.bg0,
+                    bg = palette.bg0,
+                }
+
+                hl.SignColumn = { bg = "NONE" }
+                hl.FoldColumn = { bg = "NONE" }
+                hl.LineNr = { bg = "NONE" }
+                hl.CursorLineNr = { bg = "NONE" }
+
+                hl["@comment"] = { italic = true }
+                hl["@keyword"] = { italic = false }
+                hl["@function"] = { italic = false }
+                hl["@keyword.return"] = { italic = false }
+                hl["@keyword.function"] = { italic = false }
+            end,
         })
-        vim.cmd("colorscheme kanagawa-dragon")
-    end
+
+        vim.cmd.colorscheme("everforest")
+    end,
 }
