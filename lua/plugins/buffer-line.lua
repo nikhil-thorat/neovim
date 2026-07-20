694f075ffb5fa152fcb1529fb071986998c9aeec
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-07-20 22:37:57 +0530

    changed theme from kanagawa to vscode-dark

--- lua/plugins/buffer-line.lua
+++ lua/plugins/buffer-line.lua
@@ -3,13 +3,31 @@ return {
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
+        local colors = require("vscode.colors").get_colors()
         local bufferline = require("bufferline")
         bufferline.setup {
             options = {
                 style_preset = {
                     bufferline.style_preset.minimal,
                 },
-            }
+            },
+            highlights = {
+                background = {
+                    fg = colors.vscFront,
+                    bg = "NONE",
+                },
+
+                buffer_visible = {
+                    fg = colors.vscFront,
+                    bg = "NONE",
+                },
+
+                buffer_selected = {
+                    fg = colors.vscFront,
+                    bg = "NONE",
+                    bold = true,
+                },
+            },
         }
     end
 }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,127 +1,169 @@
 return {
-    "rebelot/kanagawa.nvim",
+    "Mofiqul/vscode.nvim",
     priority = 1000,
     lazy = false,
 
     config = function()
-        local colors = require("kanagawa.colors").setup({ theme = "dragon" })
+        local c = require("vscode.colors").get_colors()
 
-        require("kanagawa").setup({
-            theme = "dragon",
+        require("vscode").setup({
             transparent = true,
-            terminalColors = true,
+            terminal_colors = true,
+            italic_comments = true,
+            disable_nvimtree_bg = true,
 
-            commentStyle = { italic = true },
-            keywordStyle = {},
-            functionStyle = {},
+            group_overrides = {
+                NormalFloat = {
+                    fg = c.vscFront,
+                    bg = c.vscPopupBack,
+                },
 
-            overrides = function()
-                return {
-                    NormalFloat = {
-                        fg = colors.theme.ui.fg,
-                        bg = colors.theme.ui.bg_m3,
-                    },
+                FloatBorder = {
+                    fg = c.vscAccentBlue,
+                    bg = c.vscPopupBack,
+                },
 
-                    FloatBorder = {
-                        fg = colors.palette.dragonBlue,
-                        bg = colors.theme.ui.bg_m3,
-                    },
+                FloatTitle = {
+                    fg = c.vscAccentBlue,
+                    bg = c.vscPopupBack,
+                    bold = true,
+                },
 
-                    FloatTitle = {
-                        fg = colors.palette.dragonPink,
-                        bg = colors.theme.ui.bg_m3,
-                        bold = true,
-                    },
+                SignColumn = { bg = "NONE" },
+                FoldColumn = { bg = "NONE" },
+                LineNr = { bg = "NONE" },
+                CursorLineNr = { bg = "NONE" },
 
-                    SignColumn = { bg = "NONE" },
-                    FoldColumn = { bg = "NONE" },
-                    LineNr = { bg = "NONE" },
-                    CursorLineNr = { bg = "NONE" },
+                TabLine = {
+                    bg = "NONE",
+                },
 
-                    LazyNormal = {
-                        bg = colors.theme.ui.bg_m3,
-                        fg = colors.theme.ui.fg_dim,
-                    },
+                TabLineFill = {
+                    bg = "NONE",
+                },
 
-                    MasonNormal = {
-                        bg = colors.theme.ui.bg_m3,
-                        fg = colors.theme.ui.fg_dim,
-                    },
+                TabLineSel = {
+                    bg = "NONE",
+                },
 
-                    Pmenu = {
-                        fg = colors.theme.ui.fg,
-                        bg = colors.theme.ui.bg_p1,
-                    },
+                BufferLineFill = {
+                    bg = "NONE",
+                },
 
-                    PmenuSel = {
-                        fg = colors.theme.ui.bg,
-                        bg = colors.palette.dragonBlue,
-                    },
+                BufferLineBackground = {
+                    bg = "NONE",
+                },
 
-                    PmenuSbar = {
-                        bg = colors.theme.ui.bg_p2,
-                    },
+                BufferLineBufferVisible = {
+                    bg = "NONE",
+                },
 
-                    PmenuThumb = {
-                        bg = colors.theme.ui.bg_p1,
-                    },
+                BufferLineBufferSelected = {
+                    bg = "NONE",
+                    bold = true,
+                },
 
-                    TelescopeTitle = {
-                        fg = colors.palette.dragonPink,
-                        bold = true,
-                    },
+                BufferLineSeparator = {
+                    fg = c.vscBack,
+                    bg = "NONE",
+                },
 
-                    TelescopePromptNormal = {
-                        bg = colors.theme.ui.bg_p1,
-                    },
+                BufferLineSeparatorVisible = {
+                    fg = c.vscBack,
+                    bg = "NONE",
+                },
 
-                    TelescopePromptBorder = {
-                        fg = colors.theme.ui.bg_p1,
-                        bg = colors.theme.ui.bg_p1,
-                    },
+                BufferLineSeparatorSelected = {
+                    fg = c.vscBack,
+                    bg = "NONE",
+                },
 
-                    TelescopeResultsNormal = {
-                        fg = colors.theme.ui.fg_dim,
-                        bg = colors.theme.ui.bg_m3,
-                    },
+                BufferLineIndicatorSelected = {
+                    fg = c.vscAccentBlue,
+                    bg = "NONE",
+                },
 
-                    TelescopeResultsBorder = {
-                        fg = colors.theme.ui.bg_m3,
-                        bg = colors.theme.ui.bg_m3,
-                    },
+                LazyNormal = {
+                    fg = c.vscFront,
+                    bg = c.vscPopupBack,
+                },
 
-                    TelescopePreviewNormal = {
-                        bg = colors.theme.ui.bg,
-                    },
+                MasonNormal = {
+                    fg = c.vscFront,
+                    bg = c.vscPopupBack,
+                },
 
-                    TelescopePreviewBorder = {
-                        fg = colors.theme.ui.bg,
-                        bg = colors.theme.ui.bg,
-                    },
+                Pmenu = {
+                    fg = c.vscFront,
+                    bg = c.vscPopupBack,
+                },
 
-                    ["@comment"] = {
-                        italic = true,
-                    },
+                PmenuSel = {
+                    fg = c.vscUiBase,
+                    bg = c.vscAccentBlue,
+                    bold = true,
+                },
 
-                    ["@keyword"] = {
-                        italic = false,
-                    },
+                PmenuSbar = {
+                    bg = c.vscScrollBar,
+                },
 
-                    ["@function"] = {
-                        italic = false,
-                    },
+                PmenuThumb = {
+                    bg = c.vscScrollBarHover,
+                },
 
-                    ["@keyword.return"] = {
-                        italic = false,
-                    },
+                TelescopeNormal = {
+                    bg = c.vscPopupBack,
+                },
 
-                    ["@keyword.function"] = {
-                        italic = false,
-                    },
-                }
-            end,
+                TelescopeBorder = {
+                    fg = c.vscPopupBack,
+                    bg = c.vscPopupBack,
+                },
+
+                TelescopePromptNormal = {
+                    bg = c.vscPopupBack,
+                },
+
+                TelescopePromptBorder = {
+                    fg = c.vscPopupBack,
+                    bg = c.vscPopupBack,
+                },
+
+                TelescopePromptTitle = {
+                    fg = c.vscAccentBlue,
+                    bg = c.vscPopupBack,
+                    bold = true,
+                },
+
+                TelescopeResultsNormal = {
+                    fg = c.vscFront,
+                    bg = c.vscPopupBack,
+                },
+
+                TelescopeResultsBorder = {
+                    fg = c.vscPopupBack,
+                    bg = c.vscPopupBack,
+                },
+
+                TelescopePreviewNormal = {
+                    bg = c.vscPopupBack,
+                },
+
+                TelescopePreviewBorder = {
+                    fg = c.vscPopupBack,
+                    bg = c.vscPopupBack,
+                },
+
+                ["@comment"] = { italic = true },
+                ["@keyword"] = { italic = false },
+                ["@function"] = { italic = false },
+                ["@keyword.return"] = { italic = false },
+                ["@keyword.function"] = { italic = false },
+            }
+
         })
 
-        vim.cmd.colorscheme("kanagawa-dragon")
+        vim.cmd.colorscheme("vscode")
     end,
 }
